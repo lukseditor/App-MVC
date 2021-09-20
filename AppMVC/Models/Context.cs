6475aaa5ae26559b8ad4244d461360c952797018
Lukseditor <lukseditor@gmail.com>
2021-09-20 05:02:33 -0300

    Aplicativo MVC

---
+++ AppMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> CategoriasProdutos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=localhost\SQLEXPRESS;Database=AppMVC;Trusted_Connection=True;");
+        }
+    }
+}
